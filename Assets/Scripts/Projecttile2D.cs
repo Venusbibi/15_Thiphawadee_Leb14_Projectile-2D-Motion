c28054148a34576f5d4c4c21b2de44c5aad4094e
Thiphawadee Tonpho <thiphawadee.tonp@bumail.net>
2026-04-30 03:34:28 +0700

    Raycas1

---
+++ Assets/Scripts/Projecttile2D.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Shooter : MonoBehaviour
+{
+   [SerializeField] Transform ShootPoint;
+   [SerializeField] GameObject target;
+   [SerializeField] Rigidbody2D  bulletPrefab;
+   
+    void Update()
+    {
+        Vector2 screenPos = Mouse.current.position.ReadValue();
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Ray ray =   Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction *5f, Color.red, 5f);
+
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if (hit.collider != null)
+            {
+
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log($"Hit{hit.collider.gameObject.name}");
+            }
+        }
+    }
+
+    
+}
+ss
